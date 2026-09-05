b583b22fff0089b6dd29002a9be65811df724cd8
jeansudrn <jeansudrn@gmail.com>
2026-09-05 18:37:57 -0300

    atualização exercicios

--- semana01/Exercicio4/Program.cs
+++ semana01/Exercicio4/Program.cs
@@ -4,6 +4,51 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Olá, Mundo! Este é o Projeto Exercicio4.");
+        List<int> numeros = new List<int>();
+        
+        
+        int numeroUsuario = -1;
+        while (numeroUsuario != 0)
+        {
+            Console.Write("Digite um número (0 para sair): ");
+            
+            string respostaUsuario = Console.ReadLine();
+            numeroUsuario = int.Parse(respostaUsuario);
+            
+            
+            if (numeroUsuario != 0)
+            {
+                numeros.Add(numeroUsuario);
+            }
+        }
+
+        // Part 1: Calcule a soma
+        int soma = 0;
+        foreach (int numero in numeros)
+        {
+            soma += numero;
+        }
+
+        Console.WriteLine($"A soma é: {soma}");
+
+        
+        
+        float media = ((float)soma) / numeros.Count;
+        Console.WriteLine($"A média é: {media}");
+
+       
+        
+        int maior = numeros[0];
+
+        foreach (int numero in numeros)
+        {
+            if (numero > maior)
+            {
+                
+                maior = numero;
+            }
+        }
+
+        Console.WriteLine($"O maior valor é: {maior}");
     }
 }
